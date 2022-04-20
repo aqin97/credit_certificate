cf99d0b0755f0e8ea6292b87cc9111cbe44d847e
aqin97 <791041423@qq.com>
2022-04-20 21:22:50 +0800

    .

--- student-api/internal/logic/studentInfoLogic.go
+++ student-api/internal/logic/studentInfoLogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 
 	"credit_certificate/student-api/internal/svc"
 	"credit_certificate/student-api/internal/types"
@@ -25,6 +26,26 @@ func NewStudentInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Stude
 
 func (l *StudentInfoLogic) StudentInfo(req *types.StudentInfoReq) (resp *types.StudentInfoResp, err error) {
 	// todo: add your logic here and delete this line
-
-	return
+	student1 := types.Student{
+		Id:     1,
+		Name:   "小钱",
+		Gender: 1,
+		Mobile: "159 8888 8888",
+	}
+	student2 := types.Student{
+		Id:     2,
+		Name:   "小李",
+		Gender: 0,
+		Mobile: "159 8888 8899",
+	}
+	m := map[int64]types.Student{
+		1: student1,
+		2: student2,
+	}
+	if studentInfo, ok := m[req.StudentId]; ok {
+		return &types.StudentInfoResp{
+			StudentInfo: studentInfo,
+		}, nil
+	}
+	return nil, errors.New("该学生不存在")
 }
